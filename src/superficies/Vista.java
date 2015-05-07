daae6057f1bdc1498df6c41b99a873b75fada70c
rpachotome <rpachotome@atanasoft01.danielcastelao.org>
2015-05-07 09:07:44 +0200

    Vista terminado

--- src/superficies/Vista.java
+++ src/superficies/Vista.java
@@ -13,6 +13,15 @@ package superficies;
 public class Vista {
     
     public void verCuadrado(Modelo mod){
-        
+        System.out.println("Si el lado es: "+mod.getLength()+" el area sera: "+mod.getArea());
+    }
+    public void verRectangulo(Modelo mod){
+        System.out.println("Si la base es: "+mod.getLength()+" y la altura: "+mod.getHeight()+" el area sera: "+mod.getArea());
+    }
+    public void verTriangulo(Modelo mod){
+        System.out.println("Si la base es: "+mod.getLength()+" y la altura: "+mod.getHeight()+" el area sera: "+mod.getArea());
+    }
+    public void verCirculo(Modelo mod){
+        System.out.println("Si el radio es: "+mod.getLength()+" el area sera: "+mod.getArea());
     }
 }
